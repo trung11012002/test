b6e00d1478976a0f6dc745403a0a8b76d3faa2eb
Trung <vuquangtrung098765@gmail.com>
2023-11-07 15:30:13 +0700

    first

---
+++ CSS/freadymath.js
@@ -0,0 +1,8 @@
+function so1(id){
+    let x = document.getElementById("so1");
+    console.log(10);
+    // x.innerText =`${Math.floor(Math.random() * 100) + 1}`;
+}
+function all() {
+    so1("so1");
+}
